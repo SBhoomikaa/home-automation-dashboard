437e1f339a6ad3e4fddfa19796d9f84756b099fb
RD <rakshitha@Rakshithas-MacBook-Air.local>
2025-06-24 00:50:59 +0530

    🔧 Split Dialogflow key into environment variables and updated index.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: processa.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.VITE_FIREBASE_APP_ID
 };
 
